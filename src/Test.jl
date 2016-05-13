2e2e918500bf0e707922038f50b8a1fb8bb7b90c
Jussi Piitulainen <jpiitula@ling.helsinki.fi>
2016-05-13 12:45:18 +0300

    Moved many tests to their proper place
    Rest to follow. Also, become nicer this way.

--- src/Test.jl
+++ src/Test.jl
@@ -9,78 +9,6 @@ function gradeln(expression, success)
     println("$(G[success])\t$expression")
 end
 
-function latticy(t)
-    println("=== Boolean lattice ===")
-    x, y, z = sort(collect(keys(t)))
-    u, v, w = t[x], t[y], t[z]
-    print("let $x = ") ; showcompact(u) ; println()
-    print("let $y = ") ; showcompact(v) ; println()
-    print("let $z = ") ; showcompact(w) ; println()
-    gradeln("$x == $x", u == u)
-    gradeln("$x ⊆ $x", u ⊆ u)
-    gradeln("$x ⊆ $x ∪ $z", u ⊆ u ∪ w)
-    gradeln("$x ∩ $z ⊆ $x", u ∩ w ⊆ u)
-    gradeln("$x ∩ $x == $x", u ∩ u == u)
-    gradeln("$x ∪ $x == $x", u ∪ u == u)
-    gradeln("$x ∩ one($x) == $x", u ∩ one(u) == u)
-    gradeln("$x ∪ zero($x) == $x", u ∪ zero(u) == u)
-    gradeln("$x - $x == zero($x)", u - u == zero(u))
-    gradeln("$x - zero($x) == $x", u - zero(u) == u)
-    gradeln("$x ∩ ~$x == zero($x)", u ∩ ~u == zero(u))
-    gradeln("$x ∪ ~$x == one($x)", u ∪ ~u == one(u))
-    gradeln("~~$x == $x", ~~u == u)
-    gradeln("$x ∩ $z == $z ∩ $x", u ∩ w == w ∩ u)
-    gradeln("$x ∪ $z == $z ∪ $x", u ∪ w == w ∪ u)
-    gradeln("$x ∩ ($x ∪ $z) == $x", u ∩ (u ∪ w) == u)
-    gradeln("$x ∪ ($x ∩ $z) == $x", u ∪ (u ∩ w) == u)
-    gradeln("$x ∩ ($y ∪ $z) == ($x ∩ $y) ∪ ($x ∩ $z)",
-            u ∩ (v ∪ w) == (u ∩ v) ∪ (u ∩ w))
-    gradeln("$x ∪ ($y ∩ $z) == ($x ∪ $y) ∩ ($x ∪ $z)",
-            u ∪ (v ∩ w) == (u ∪ v) ∩ (u ∪ w))
-    gradeln("$x ∩ ($y ∩ $z) == ($x ∩ $y) ∩ $z",
-            u ∩ (v ∩ w) == (u ∩ v) ∩ w)
-    gradeln("$x ∪ ($y ∪ $z) == ($x ∪ $y) ∪ $z",
-            u ∪ (v ∪ w) == (u ∪ v) ∪ w)
-    println()
-end
-
-function transposy(R)
-    println("== Opposite relation, indexing ===")
-    print("let R = ") ; showcompact(R) ; println()
-    gradeln("R'' == R", R'' == R)
-    println("k ∈ R[j,:] for j in 1:3, k in 1:3")
-    gradeln("<=> j ∈ R[:,k]",
-            all(Bool[(k ∈ R[j,:]) == (j ∈ R[:,k]) # need the ()!
-                     for j in 1:3, k in 1:3]))
-    gradeln("<=> (j,k) ∈ R",
-            all(Bool[(j ∈ R[:,k]) == ((j,k) ∈ R)
-                     for j in 1:3, k in 1:3]))
-    gradeln("<=> (k,j) ∈ R'",
-            all(Bool[((j,k) ∈ R) == ((k,j) ∈ R')
-                     for j in 1:3, k in 1:3]))
-    gradeln("<=> j ∈ R'[k,:]",
-            all(Bool[((k,j) ∈ R') == (j ∈ R'[k,:])
-                     for j in 1:3, k in 1:3]))
-    gradeln("<=> k ∈ R'[:,j]",
-            all(Bool[(j ∈ R'[k,:]) == (k ∈ R'[:,j])
-                     for j in 1:3, k in 1:3]))
-    println()
-end
-
-function monoidy(t)
-    println("=== Composition is a monoid ===")
-    x, y, z = sort(collect(keys(t)))
-    u, v, w = t[x], t[y], t[z]
-    print("let $x = ") ; showcompact(u) ; println()
-    print("let $y = ") ; showcompact(v) ; println()
-    print("let $z = ") ; showcompact(w) ; println()
-    gradeln("eye($x) ∘ $x == $x == $x ∘ eye($x)",
-            eye(u) ∘ u == u == u ∘ eye(u))
-    gradeln("$x ∘ ($y ∘ $z) == ($x ∘ $y) ∘ $z",
-            u ∘ (v ∘ w) == (u ∘ v) ∘ w)
-    println()
-end
-
 function producty(y, z)
     println("== Product, each of part, opposite, projection ===")
     print("let y = ") ; showcompact(y) ; println()
@@ -170,15 +98,13 @@ let
 pd = Dict(:x => randp(3),
           :y => randp(3),
           :z => randp(3))
-latticy(pd)
+
 producty(pd[:y], pd[:z])
 
 rd = Dict(:r => randr(3),
           :s => randr(3),
           :t => randr(3))
-latticy(rd)
-monoidy(rd)
-transposy(rd[:r])
+
 pointy(pd[:x], rd[:r])
 diagonally(pd[:x], rd[:r])
 eachy(pd[:x], rd[:r])
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,80 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+function lattice_tests(x, y, z)
+    @test x == x
+    @test zero(x) ⊆ x ⊆ one(x)
+    @test x ∩ x == x 
+    @test x ∪ x == x
+    @test x ∩ z == z ∩ x
+    @test x ∪ z == z ∪ x
+    @test x ∩ (y ∩ z) == (x ∩ y) ∩ z
+    @test x ∪ (y ∪ z) == (x ∪ y) ∪ z
+    @test x ∩ (y ∪ z) == (x ∩ y) ∪ (x ∩ z)
+    @test x ∪ (y ∩ z) == (x ∪ y) ∩ (x ∪ z)
+    @test x ∩ (x ∪ z) == x
+    @test x ∪ (x ∩ z) == x
+    @test x ∩ one(x) == x
+    @test x ∩ zero(x) == zero(x)
+    @test x ∪ one(x) == one(x)
+    @test x ∪ zero(x) == x
+    @test x - zero(x) == x
+    @test zero(x) - x == zero(x)
+    @test x - x == zero(x)
+    @test x - one(x) == zero(x)
+    @test one(x) - x == ~x
+    @test x - z == x ∩ ~z
+    @test ~~x == x
+    @test ~(x ∩ z) == ~x ∪ ~z
+    @test ~(x ∪ z) == ~x ∩ ~z
+    @test x ∩ ~x == zero(x)
+    @test x ∪ ~x == one(x)
+    @test x ∩ z ⊆ x
+    @test x ⊆ x ∪ z
+end
+
+function composition_tests(r, s, t)
+    @test eye(r) ∘ r == r == r ∘ eye(r)
+    @test r ∘ (s ∘ t) == (r ∘ s) ∘ t
+    @test (r ∘ s)' == s' ∘ r'
+end
+
+function indexing_tests{N}(r::TinyRelation{N})
+    for j in 1:N, k in 1:N
+        @test ((j,k) ∈ r) == (r[j,k] == 1)
+        @test ((j,k) ∈ r) == (k ∈ r[j,:]) == (r[j,:][k] == 1)
+        @test ((j,k) ∈ r) == (j ∈ r[:,k]) == (r[:,k][j] == 1)
+    end
+end
+
+function opposite_tests{N}(r::TinyRelation{N}, a::TinyPart{N})
+    @test r == r''
+    @test a ∘ r == r' ∘ a
+    for j in 1:N, k in 1:N
+        @test ((j,k) ∈ r) == ((k,j) ∈ r')
+    end
+    for k in 1:N
+        @test r[k,:] == r'[:,k]
+    end
+end
+
+let
+    a, b, c = randp(3), randp(3), randp(3)
+    r, s, t = randr(3), randr(3), randr(3)
+
+    info("Lattice tests on random 3-parts")
+    lattice_tests(a, b, c)
+
+    info("Lattice tests on random 3-relations")
+    lattice_tests(r, s, t)
+
+    info("Composition tests on random 3-relations")
+    composition_tests(r, s, t)
+
+    info("Indexing tests on a random 3-relation")
+    indexing_tests(r)
+
+    info("Opposite test on a random 3-relation and 3-part")
+    opposite_tests(r, a)
+end
