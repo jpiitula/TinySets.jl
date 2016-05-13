ce7ef06a91b8e4af654057c8fefc6b526ea1289a
Jussi Piitulainen <jpiitula@ling.helsinki.fi>
2016-05-13 13:59:54 +0300

    Rest tests in their proper place (in test/runtests.jl)

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,6 +42,15 @@ function composition_tests(r, s, t)
     @test (r ∘ s)' == s' ∘ r'
 end
 
+function diagonal_tests{N}(A::TinyPart{N}, R::TinyRelation{N})
+    @test diagr(zero(A)) == zero(R)
+    @test diagr(one(A)) == eye(A) == eye(R)
+    @test diag(zero(R)) == zero(A)
+    @test diag(one(R)) == one(A)
+    @test diag(diagr(A)) == A
+    @test diagr(diag(R)) ⊆ R
+end
+
 function indexing_tests{N}(r::TinyRelation{N})
     for j in 1:N, k in 1:N
         @test ((j,k) ∈ r) == (r[j,k] == 1)
@@ -61,6 +70,45 @@ function opposite_tests{N}(r::TinyRelation{N}, a::TinyPart{N})
     end
 end
 
+function product_tests(A, B)
+    @test A × B == reduce(∪, zero(A × B),
+                          map(k -> k × B,
+                              each(Int, A)))
+    @test A × B == reduce(∪, zero(A × B),
+                          map(k -> A × k,
+                              each(Int, B)))
+    @test A × B == (B × A)'
+    @test zero(A) × B == zero(A × B) == A × zero(B)
+    @test one(A) × one(B) == one(A × B)
+    @test B == zero(B) || A == (A × B) ∘ one(B)
+    @test A == zero(A) || B == one(A) ∘ (A × B)
+end
+
+function each_tests{N}(A::TinyPart{N}, R::TinyRelation{N})
+    @test length(zero(A)) == length(Set(each(Int, zero(A)))) == 0
+    @test length(zero(R)) == length(Set(each(Int, zero(R)))) == 0
+    @test length(one(A)) == length(Set(each(Int, one(A)))) == N
+    @test length(one(R)) == length(Set(each(Int, one(R)))) == N^2
+    @test length(TinyPart{N}) == 2^N
+    @test length(TinyRelation{N}) == 2^(N^2)
+    @test length(A) == length(Set(each(Int, A)))
+    @test length(R) == length(Set(each(Int, R)))
+    @test length(A) == length(Set(each(Char, A)))
+    @test length(R) == length(Set(each(Char, R)))
+    @test length(A) == length(Set(each(Symbol, A)))
+    @test length(R) == length(Set(each(Symbol, R)))
+end
+
+function point_tests(A, R)
+    @test A == reduce(∪, zero(A),
+                      map(k -> point(A, k),
+                          each(Int, A)))
+    @test R == reduce(∪, zero(R),
+                      map(p -> point(R, p...),
+                          each(Int, R)))
+
+end
+
 let
     a, b, c = randp(3), randp(3), randp(3)
     r, s, t = randr(3), randr(3), randr(3)
@@ -74,9 +122,21 @@ let
     info("Composition tests on random 3-relations")
     composition_tests(r, s, t)
 
+    info("Diagonal tests on a random 3-part and 3-relation")
+    diagonal_tests(a, r)
+
     info("Indexing tests on a random 3-relation")
     indexing_tests(r)
 
-    info("Opposite test on a random 3-relation and 3-part")
+    info("Opposite tests on a random 3-relation and 3-part")
     opposite_tests(r, a)
+
+    info("Product tests on random 3-parts")
+    product_tests(a, b)
+
+    info("Each-tests on a random 3-part and 3-relation")
+    each_tests(a, r)
+
+    info("Point-tests on a random 3-part and 3-relation")
+    point_tests(a, r)
 end
